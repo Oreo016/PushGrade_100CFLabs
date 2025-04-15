5d5194c90ebe0fcaccc97aacf8f9c076b89f5332
Oreo <188584108+Oreo016@users.noreply.github.com>
2025-04-15 19:00:26 +0530

    Update Basic_syntax_and_data_types.py

--- 1.Introduction to Python/Basic_syntax_and_data_types.py
+++ 1.Introduction to Python/Basic_syntax_and_data_types.py
@@ -1,1 +1,31 @@
+#This is start of PushGrade course 
+#These are single line comments
+'''It is a multi line
+comment '''
+if True:
+  print("This block executed as condition is true")
+if False:
+  #This block will not be executed
+  print("Not executed")
+x=10 #x is a variable and is assigned value of 5 which is a statement
+y=x+20 #it is an expression 
+a=20 #integer
+b=2.71 #float
+c="Hello" #string
+l=[1,2,3] #list
+d = {1:"One",2:"Two"} #dictionary
+
+
+#Project 1 
+#Basic python program to calculate sum of given numbers
+i=int(input("Please enter no of values to add"))
+l=[]
+for k in range(i):
+  l.append(int(input(f"Enter {k+1}th value to add "))
+print("Sum of all elements ",sum(l))
+
+
+
+
+
 
